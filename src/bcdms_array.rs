c66a94c5cab08c12f6b93d9a6eb35df54514a788
Daniel A. Mock <qwertz120@gmail.com>
2018-09-12 11:11:23 +0200

    init

---
+++ src/bcdms_array.rs
@@ -0,0 +1,150 @@
+#[derive(Debug, Clone)]
+pub struct BcdmsArray<T> {
+    index: Vec<Vec<T>>, // the index pointing to the data blocks,
+
+    // TODO: maybe compute ad hoc, could be quite expensive (high constant)
+    n: usize, // number of elements
+    d: usize, // the number of non-empty data blocks
+
+    // TODO Maybe replace with bool
+    s: usize, // number of suberblocks
+
+    // TODO: maybe replace with call to index[d] or index.len
+    has_empty_data: bool, // the number of empty data blocks (there are atmost two)
+
+    // TODO: replace it with vector calls do index[d-1]
+    len_last_data: usize, // occupancy of last data block
+    cap_last_data: usize, // size of last data block
+    
+    // Definitely needed IMO
+    len_last_super: usize, // length of super block (amount of data blocks)
+    cap_last_super: usize, // capacity of super block (amount of data blocks)
+}
+
+impl<T> BcdmsArray<T> {
+    pub fn new() -> BcdmsArray<T> {
+        BcdmsArray {
+            index: vec![Vec::with_capacity(1)],
+            n: 0,
+            s: 1,
+            d: 1,
+            has_empty_data: false,
+            len_last_data: 0,
+            cap_last_data: 1,
+            len_last_super: 1,
+            cap_last_super: 1,
+        }
+    }
+
+    pub fn push(&mut self, value: T) {
+        self.grow();
+        self.index[self.d-1].push(value);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.shrink()
+    }
+
+    pub fn read(&self, i: usize) -> Option<&T> {
+        if i >= self.n {
+            return None
+        } 
+        let (a,b) = BcdmsArray::<T>::locate(i);
+        println!("{}, {}", a,b);
+        Some(&self.index[a][b])
+    }
+
+    fn grow(&mut self) {
+        // 1. If the last nonempty data block DB[d-1] is full
+        if self.cap_last_data == self.len_last_data {
+            // a If the last superblock SB[s-1] is full
+            if self.len_last_super == self.cap_last_super {
+                self.s += 1;
+                if self.s % 2 == 1 {
+                    self.cap_last_super *= 2;
+                } else {
+                    self.cap_last_data *= 2;
+                }
+                self.len_last_super = 0;
+            }
+
+            if !self.has_empty_data {
+                // i manual resizing is unnecessary
+                self.index.push(Vec::with_capacity(self.cap_last_data));
+            } else {
+                self.has_empty_data = false;
+            }
+
+            self.d += 1;
+            self.len_last_super += 1;
+            self.len_last_data = 0;
+        }
+
+        self.n += 1;
+        self.len_last_data += 1;
+    }
+    
+
+    fn shrink(&mut self) -> Option<T> {
+        if self.n == 0 {
+            return None
+        }
+
+        self.n -= 1;
+        self.len_last_data -= 1;
+        let ret = self.index[self.d-1].pop();
+
+        // If DB[d-1] is empty
+        if  self.len_last_data == 0 {
+            // If there is another empty data block, it has to be at last position (DB[d]), remove it with pop
+            if self.has_empty_data {
+                let temp = self.index.pop();
+                assert!(
+                    match temp {
+                        None => true,
+                        Some(i) => i.is_empty(),
+                    });
+            }
+
+            // 2 b TODO reallocate index when quarter full???
+
+            self.d -= 1;
+            self.len_last_super -= 1;
+
+            if self.len_last_super == 0 {
+                self.s -= 1;
+                
+                if self.s % 2 == 0 {
+                    self.cap_last_super /= 2;
+                } else {
+                    self.cap_last_data /= 2;
+                }
+
+                self.len_last_super = self.cap_last_super;
+            }
+
+            self.len_last_data = self.cap_last_data;
+
+        }
+        
+        return ret
+    }
+
+    fn locate(index: usize) -> (usize, usize) {
+        let index = index + 1;
+        
+        let k = usize::max_value().count_ones() - index.leading_zeros() - 1; // size of index - 1
+        
+        let l = (k + 1) / 2; // ceil(k/2)
+
+        let b = (index & !usize::pow(2, k)) >> l;
+
+        let e = index & (usize::pow(2, l) - 1); // get the last ceil(k/2) bits of index+1
+
+        // There is an error in the paper. The number of data blocks in super blocks prior to SB[k] is not 2^k - 1, since an SB[i] has 2^floor(k/2) data blocks, not 2^i
+        let p = usize::pow(2, (k + 1) / 2 + 1) - 2 - if k % 2 == 1 {usize::pow(2, (k-1)/2)} else {0}; 
+        
+        // return e-th element of DB[p+b]
+        (p + b, e)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+mod bcdms_array;
+
+fn main() {
+    println!("Hello, world!");
+
+    let mut a = bcdms_array::BcdmsArray::new();
+    for i in 0..1000 {
+        a.push(i);
+        println!("{}", a.read(i).unwrap());
+    }
+
+    for i in 0..1000 {
+        println!("{:?}", a.pop());
+    }
+}
