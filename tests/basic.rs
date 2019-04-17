2d5f35ce0d304e8f7b0b516eeb97b5920f62f303
whentze <muvlon@hentzes.de>
2019-04-17 16:25:48 +0200

    turn into library crate and move main.rs to integration test

---
+++ tests/basic.rs
@@ -0,0 +1,21 @@
+#[test]
+fn push_iter_pop() {
+    let mut a = bcdms_array::BcdmsArray::new();
+    
+    for i in 0..10 {
+        a.push(i);
+        assert_eq!(a.read(i).unwrap(), &i);
+    }
+
+    for element in a.iter_mut() {
+        *element += 1;
+    }
+
+    a.iter().enumerate().for_each(|(i, x)| assert_eq!(*x, i+1));
+
+    for i in 0..10 {
+        assert_eq!(a.pop().unwrap(), 10-i);
+    }
+    
+    assert_eq!(a.pop(), None);
+}
