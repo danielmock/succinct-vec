186a3632218cf1a2106c4589ccbd65d7b654d41c
whentze <muvlon@hentzes.de>
2019-04-17 20:10:25 +0200

    add more benches

--- benches/vec_compare.rs
+++ benches/vec_compare.rs
@@ -3,15 +3,84 @@ extern crate test;
 
 use test::{black_box, Bencher};
 
+use lazy_static::lazy_static;
+use rand::prelude::*;
+use bcdms_array::BcdmsArray;
+
 const BENCH_LEN: usize = 1_000_000;
 
-#[bench]
-fn index_bcdms(b: &mut Bencher) {
-    let mut bcdms = bcdms_array::BcdmsArray::default();
+lazy_static! {
+    static ref TEST_BCDMS : BcdmsArray<usize> = {
+        let mut bcdms = BcdmsArray::default();
 
-    for i in 0..BENCH_LEN {
-        bcdms.push(i);
-    }
+        for i in 0..BENCH_LEN {
+            bcdms.push(i);
+        }
+        bcdms
+    };
+
+    static ref TEST_VEC : Vec<usize> = {
+        let mut vec = Vec::default();
+
+        for i in 0..BENCH_LEN {
+            vec.push(i);
+        }
+        vec
+    };
+
+    static ref RANDOM_INDEXES : Vec<usize> = {
+        // deterministic seed for reproducibility
+        let mut rng = SmallRng::seed_from_u64(123456);
+
+        let mut idxs = Vec::with_capacity(BENCH_LEN);
+
+        for _ in 0..BENCH_LEN {
+            idxs.push(rng.gen_range(0, BENCH_LEN));
+        }
+        idxs
+    };
+}
+
+
+#[bench]
+fn clone_bcdms(b: &mut Bencher) {
+    let bcdms = &*TEST_BCDMS;
+    b.iter(|| {
+        black_box(black_box(bcdms).clone())
+    });
+}
+
+#[bench]
+fn clone_vec(b: &mut Bencher) {
+    let vec = &*TEST_VEC;
+    b.iter(|| {
+        black_box(black_box(vec).clone())
+    });
+}
+
+#[bench]
+fn iter_bcdms(b: &mut Bencher) {
+    let bcdms = TEST_BCDMS.clone();
+    b.iter(|| {
+        for i in bcdms.iter() {
+            black_box(i);
+        }
+    });
+}
+
+#[bench]
+fn iter_vec(b: &mut Bencher) {
+    let vec = TEST_VEC.clone();
+    b.iter(|| {
+        for i in vec.iter() {
+            black_box(i);
+        }
+    });
+}
+
+#[bench]
+fn index_seq_bcdms(b: &mut Bencher) {
+    let bcdms = TEST_BCDMS.clone();
 
     b.iter(|| {
         for i in 0..BENCH_LEN {
@@ -21,16 +90,34 @@ fn index_bcdms(b: &mut Bencher) {
 }
 
 #[bench]
-fn index_vec(b: &mut Bencher) {
-    let mut vec = Vec::default();
-
-    for i in 0..BENCH_LEN {
-        vec.push(i);
-    }
+fn index_seq_vec(b: &mut Bencher) {
+    let vec = TEST_VEC.clone();
 
     b.iter(|| {
         for i in 0..BENCH_LEN {
             black_box(vec[black_box(i)]);
+        }
+    });
+}
+
+#[bench]
+fn index_rand_bcdms(b: &mut Bencher) {
+    let idxs = &*RANDOM_INDEXES;
+    let bcdms = TEST_BCDMS.clone();
+    b.iter(|| {
+        for i in idxs {
+            black_box(bcdms[black_box(*i)]);
+        }
+    });
+}
+
+#[bench]
+fn index_rand_vec(b: &mut Bencher) {
+    let idxs = &*RANDOM_INDEXES;
+    let vec = TEST_VEC.clone();
+    b.iter(|| {
+        for i in idxs {
+            black_box(vec[black_box(*i)]);
         }
     });
 }
