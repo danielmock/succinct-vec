40918e9184ef7277de0c729825a8b5ae73d357ac
whentze <muvlon@hentzes.de>
2019-04-17 18:34:50 +0200

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -17,17 +17,16 @@ pub struct BcdmsArray<T> {
     // TODO: replace it with vector calls do index[d-1]
     len_last_data: usize, // occupancy of last data block
     cap_last_data: usize, // size of last data block
-    
+
     // Definitely needed IMO
     len_last_super: usize, // length of super block (amount of data blocks)
     cap_last_super: usize, // capacity of super block (amount of data blocks)
 }
 
 impl<T> BcdmsArray<T> {
-
     pub fn push(&mut self, value: T) {
         self.grow();
-        self.index[self.d-1].push(value);
+        self.index[self.d - 1].push(value);
     }
 
     pub fn pop(&mut self) -> Option<T> {
@@ -36,9 +35,9 @@ impl<T> BcdmsArray<T> {
 
     pub fn read(&self, i: usize) -> Option<&T> {
         if i >= self.n {
-            return None
-        } 
-        let (a,b) = BcdmsArray::<T>::locate(i);
+            return None;
+        }
+        let (a, b) = BcdmsArray::<T>::locate(i);
         Some(&self.index[a][b])
     }
 
@@ -71,27 +70,25 @@ impl<T> BcdmsArray<T> {
         self.n += 1;
         self.len_last_data += 1;
     }
-    
 
     fn shrink(&mut self) -> Option<T> {
         if self.n == 0 {
-            return None
+            return None;
         }
 
         self.n -= 1;
         self.len_last_data -= 1;
-        let ret = self.index[self.d-1].pop();
+        let ret = self.index[self.d - 1].pop();
 
         // If DB[d-1] is empty
-        if  self.len_last_data == 0 {
+        if self.len_last_data == 0 {
             // If there is another empty data block, it has to be at last position (DB[d]), remove it with pop
             if self.has_empty_data {
                 let temp = self.index.pop();
-                assert!(
-                    match temp {
-                        None => true,
-                        Some(i) => i.is_empty(),
-                    });
+                assert!(match temp {
+                    None => true,
+                    Some(i) => i.is_empty(),
+                });
             }
 
             // 2 b TODO reallocate index when quarter full???
@@ -101,7 +98,7 @@ impl<T> BcdmsArray<T> {
 
             if self.len_last_super == 0 {
                 self.s -= 1;
-                
+
                 if self.s % 2 == 0 {
                     self.cap_last_super /= 2;
                 } else {
@@ -112,29 +109,25 @@ impl<T> BcdmsArray<T> {
             }
 
             self.len_last_data = self.cap_last_data;
-
         }
-        
-        return ret
+
+        return ret;
     }
 
     fn locate(index: usize) -> (usize, usize) {
         let index = index + 1;
 
-        let k = std::mem::size_of::<usize>()*8 - index.leading_zeros() as usize - 1; // size of index - 1
+        let k = std::mem::size_of::<usize>() * 8 - index.leading_zeros() as usize - 1; // size of index - 1
 
         let l = (k + 1) / 2; // ceil(k/2)
 
         let b = (index & !(1 << k)) >> l; // get the first floor(k/2) bits of index after the leading One
-        // remember that there are k bits after the leading 1 and that we have to cut off the last ceil(k/2) bits
+                                          // remember that there are k bits after the leading 1 and that we have to cut off the last ceil(k/2) bits
 
         let e = index & ((1 << l) - 1); // get the last ceil(k/2) bits of index
 
         // There is an error in the paper. The number of data blocks in super blocks prior to SB[k] is not 2^k - 1, since an SB[i] has 2^floor(i/2) data blocks, not 2^i
-        let p = 2*((1 << l)-1)
-            - (k % 2) * (
-                (1 << l)/2
-            );
+        let p = 2 * ((1 << l) - 1) - (k % 2) * ((1 << l) / 2);
 
         // return e-th element of DB[p+b]
         (p + b, e)
@@ -168,14 +161,14 @@ impl<T> Index<usize> for BcdmsArray<T> {
     type Output = T;
 
     fn index(&self, i: usize) -> &T {
-        let (a,b) = BcdmsArray::<T>::locate(i);
+        let (a, b) = BcdmsArray::<T>::locate(i);
         &self.index[a][b]
     }
 }
 
 impl<T> IndexMut<usize> for BcdmsArray<T> {
     fn index_mut(&mut self, i: usize) -> &mut T {
-        let (a,b) = BcdmsArray::<T>::locate(i);
+        let (a, b) = BcdmsArray::<T>::locate(i);
         &mut self.index[a][b]
     }
 }
--- tests/basic.rs
+++ tests/basic.rs
@@ -1,7 +1,7 @@
 #[test]
 fn push_iter_pop() {
     let mut a = bcdms_array::BcdmsArray::default();
-    
+
     for i in 0..10 {
         a.push(i);
         assert_eq!(a[i], i);
@@ -11,11 +11,13 @@ fn push_iter_pop() {
         *element += 1;
     }
 
-    a.iter().enumerate().for_each(|(i, x)| assert_eq!(*x, i+1));
+    a.iter()
+        .enumerate()
+        .for_each(|(i, x)| assert_eq!(*x, i + 1));
 
     for i in 0..10 {
-        assert_eq!(a.pop().unwrap(), 10-i);
+        assert_eq!(a.pop().unwrap(), 10 - i);
     }
-    
+
     assert_eq!(a.pop(), None);
 }
